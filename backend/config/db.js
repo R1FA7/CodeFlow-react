fcf770bcc18c2d024578e5fe6fa943e4a769ac0e
R1FA7 <minhazulkabir510@gmail.com>
2025-11-09 21:37:03 +0600

    update mdburi

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async()=>{
   try{
-    await mongoose.connect(`${process.env.MONGODB_URI}/codeflow`)
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log("DB connected")
   } catch(err){
     console.log("DB connection failed ",err.message)
